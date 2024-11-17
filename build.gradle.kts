55b9dd2f06b88c79721de43debfebc112bd92264
Sergey Volchenko <s.volchenko@tinkoff.ru>
2024-11-17 21:07:24 +0300

    Добавил detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management")
     kotlin("jvm")
     kotlin("plugin.spring")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 allprojects {
@@ -12,14 +13,27 @@ allprojects {
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 
     repositories {
         mavenCentral()
     }
 
     dependencies {
+        val detektVersion: String by project
+
         implementation("org.springframework.boot:spring-boot-starter")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+    }
+
+    tasks.detekt {
+        autoCorrect = true
+        config = files("${project.rootDir}/config/detekt/detekt.yaml")
+    }
+
+    tasks.check {
+        dependsOn("detekt")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,14 @@ pluginManagement {
     val springBootVersion: String by settings
     val springDependencyManagementVersion: String by settings
     val kotlinVersion: String by settings
+    val detektVersion: String by settings
 
     plugins {
         id("org.springframework.boot") version springBootVersion
         id("io.spring.dependency-management") version springDependencyManagementVersion
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.spring") version kotlinVersion
+        id("io.gitlab.arturbosch.detekt") version detektVersion
     }
 }
 
