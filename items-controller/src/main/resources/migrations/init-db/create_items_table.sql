3b6548cde06e4b8d0f73d60ef5d6c595e2e117f5
aleksandrledenev <ledenevsanok@gmail.com>
2024-11-18 21:35:06 +0300

    Добавил миграции

--- items-controller/src/main/resources/migrations/init-db/create_items_table.sql
+++ items-controller/src/main/resources/migrations/init-db/create_items_table.sql
@@ -1,18 +1,18 @@
 -- Create Sequences
 -- ================================================================
-create sequence link_sequence start with 1 increment by 1;
+create sequence items_sequence start with 1 increment by 1;
 
 -- Create Tables
 -- ================================================================
 -- Document
 create table items
 (
-    id              bigint                   not null default nextVal('link_sequence'),
+    id              bigint                   not null default nextVal('items_sequence'),
     type            varchar(255)             not null,
     color           varchar(255)             null,
     status          varchar(255)             not null,
     created_at      timestamp with time zone not null,
     updated_at      timestamp with time zone null,
 
-    constraint link_pk primary key (id)
+    constraint items_pk primary key (id)
 );
