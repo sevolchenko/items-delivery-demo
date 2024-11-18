504a2941b2b1983d55af8a4640f7f66e2788c169
Sergey Volchenko <s.volchenko@tinkoff.ru>
2024-11-18 15:17:22 +0300

    Заполнил SpringDoc

--- items-controller/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/item/adapter/rest/ItemController.kt
+++ items-controller/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/item/adapter/rest/ItemController.kt
@@ -1,20 +1,54 @@
 package ru.tbank.itemsdeliverydemo.itemscontroller.item.adapter.rest
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.tbank.itemsdeliverydemo.itemscontroller.item.ItemService
+import ru.tbank.itemsdeliverydemo.itemscontroller.item.adapter.rest.dto.ErrorResponse
 import ru.tbank.itemsdeliverydemo.itemscontroller.item.adapter.rest.dto.ReserveItemResponse
 
+@Tag(name = "Items")
 @RestController
 @RequestMapping("/api/v1/items")
 class ItemController(
     private val itemService: ItemService
 ) {
 
+    @Operation(
+        summary = "Зарезервировать товар по опциональными параметрами",
+        description = "Ищет товар со статусом AVAILABLE, меняет его статус на RESERVED и возвращает его id",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Запрос успешно выполнен",
+                content = [
+                    Content(
+                        schema = Schema(implementation = ReserveItemResponse::class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Нет доступных товаров с такими параметрами",
+                content = [
+                    Content(
+                        schema = Schema(implementation = ErrorResponse::class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            )
+        ]
+    )
     @PostMapping("/reserve")
     fun reserveItem(
         @RequestParam(required = false) type: String?,
@@ -22,7 +56,7 @@ class ItemController(
     ): ResponseEntity<*> {
         val reservedItemId = itemService.reserveItem(type, color)
             ?: return ResponseEntity
-                .status(HttpStatus.NOT_FOUND).body("No available item found")
+                .status(HttpStatus.NOT_FOUND).body(ErrorResponse("No available item found"))
 
         return ResponseEntity.ok().body(ReserveItemResponse(reservedItemId))
     }
