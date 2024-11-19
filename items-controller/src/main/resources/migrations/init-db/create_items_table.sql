19108b50d02e7d2c15d7ca5d4323b401c0757a5c
aleksandrledenev <ledenevsanok@gmail.com>
2024-11-20 01:36:32 +0300

    Установил default значение null для updated_at

--- items-controller/src/main/resources/migrations/init-db/create_items_table.sql
+++ items-controller/src/main/resources/migrations/init-db/create_items_table.sql
@@ -8,7 +8,7 @@ create table item
     color           varchar(255)      null     default null,
     status          varchar(255)      not null default 'AVAILABLE',
     created_at      timestamp         not null default NOW(),
-    updated_at      timestamp         null     default NOW(),
+    updated_at      timestamp         null     default null,
 
     constraint item_pk primary key (id)
 );
