8e62994f8c3c226d28271851b712a34a92ac64d9
aleksandrledenev <ledenevsanok@gmail.com>
2024-11-20 01:00:27 +0300

    Переименовал items в item и убрал with timezone

--- items-controller/src/main/resources/migrations/init-db/create_items_table.sql
+++ items-controller/src/main/resources/migrations/init-db/create_items_table.sql
@@ -1,18 +1,18 @@
 -- Create Sequences
 -- ================================================================
-create sequence items_sequence start with 1 increment by 1;
+create sequence item_sequence start with 1 increment by 1;
 
 -- Create Tables
 -- ================================================================
 -- Document
-create table items
+create table item
 (
-    id              bigint                   not null default nextVal('items_sequence'),
-    type            varchar(255)             not null,
-    color           varchar(255)             null     default null,
-    status          varchar(255)             not null default 'AVAILABLE',
-    created_at      timestamp with time zone not null default NOW(),
-    updated_at      timestamp with time zone null     default NOW(),
+    id              bigint            not null default nextVal('item_sequence'),
+    type            varchar(255)      not null,
+    color           varchar(255)      null     default null,
+    status          varchar(255)      not null default 'AVAILABLE',
+    created_at      timestamp         not null default NOW(),
+    updated_at      timestamp         null     default NOW(),
 
-    constraint items_pk primary key (id)
+    constraint item_pk primary key (id)
 );
