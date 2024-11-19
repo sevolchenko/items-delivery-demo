6430f8d132dc886fc9b8a67b1adebb49d251c146
aleksandrledenev <ledenevsanok@gmail.com>
2024-11-19 19:38:57 +0300

    Добавил значения по умолчанию в миграции

--- items-controller/src/main/resources/migrations/init-db/create_items_table.sql
+++ items-controller/src/main/resources/migrations/init-db/create_items_table.sql
@@ -9,10 +9,10 @@ create table items
 (
     id              bigint                   not null default nextVal('items_sequence'),
     type            varchar(255)             not null,
-    color           varchar(255)             null,
-    status          varchar(255)             not null,
-    created_at      timestamp with time zone not null,
-    updated_at      timestamp with time zone null,
+    color           varchar(255)             null     default null,
+    status          varchar(255)             not null default 'AVAILABLE',
+    created_at      timestamp with time zone not null default NOW(),
+    updated_at      timestamp with time zone null     default NOW(),
 
     constraint items_pk primary key (id)
 );
