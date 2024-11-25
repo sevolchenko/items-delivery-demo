452e69e9381a867aaf640f34f4a323611be96f49
Sergey Volchenko <s.volchenko@tinkoff.ru>
2024-11-25 14:33:13 +0300

    Создал основные классы

---
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/jpa/entity/Application.kt
@@ -0,0 +1,4 @@
+package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.entity
+
+class Application {
+}
---
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/ApplicationController.kt
@@ -0,0 +1,7 @@
+package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ApplicationController {
+}
