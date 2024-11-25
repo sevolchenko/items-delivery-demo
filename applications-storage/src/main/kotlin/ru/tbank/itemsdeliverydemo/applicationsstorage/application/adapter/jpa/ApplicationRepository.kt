b0b5a46d1c4e024023529399cb7aaf270cd88b03
Sergey Volchenko <s.volchenko@tinkoff.ru>
2024-11-25 14:54:26 +0300

    Создал модель заявки

--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/jpa/ApplicationRepository.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/jpa/ApplicationRepository.kt
@@ -3,6 +3,7 @@ package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.entity.Application
+import java.util.UUID
 
 @Repository
-interface ApplicationRepository : JpaRepository<Application, Long>
+interface ApplicationRepository : JpaRepository<Application, UUID>
--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/jpa/entity/Application.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/jpa/entity/Application.kt
@@ -1,4 +1,26 @@
 package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.entity
 
-class Application {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import ru.tbank.itemsdeliverydemo.applicationsstorage.application.model.ApplicationStatus
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "application")
+data class Application(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+
+    @Enumerated(EnumType.STRING)
+    var status: ApplicationStatus = ApplicationStatus.CREATED,
+
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    var updatedAt: LocalDateTime? = null
+)
