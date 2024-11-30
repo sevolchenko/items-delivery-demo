25ced925df4f0514529f821b51a36e010ef11107
aleksandrledenev <ledenevsanok@gmail.com>
2024-12-01 02:42:04 +0300

    Поправил ошибки

--- items-controller/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/item/adapter/rest/MaintenanceController.kt
+++ items-controller/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/item/adapter/rest/MaintenanceController.kt
@@ -9,7 +9,6 @@ import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -60,6 +59,5 @@ class MaintenanceController(
                 .status(HttpStatus.BAD_REQUEST).body(ErrorResponse("Invalid request parameters"))
 
         return ResponseEntity.ok().body(CreateItemResponse(createdItemsId))
-
     }
 }
