b69b7de5e75cfa273bb7496f370b365cbfab8f4b
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-01-08 20:50:17 +0300

    Прогнал detekt

--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/ApplicationController.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/ApplicationController.kt
@@ -3,5 +3,4 @@ package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ApplicationController {
-}
+class ApplicationController
