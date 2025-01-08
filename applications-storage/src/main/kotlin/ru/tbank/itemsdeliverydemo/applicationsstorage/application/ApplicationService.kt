8a53821d38b555d6fb8de281be2e6753c8e1fac7
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-01-08 21:04:27 +0300

    Добавил автоинкремент id

--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/ApplicationService.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/ApplicationService.kt
@@ -1,7 +1,19 @@
 package ru.tbank.itemsdeliverydemo.applicationsstorage.application
 
 import org.springframework.stereotype.Service
+import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.ApplicationRepository
+import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.entity.Application
+import kotlin.jvm.optionals.getOrNull
 
 @Service
-class ApplicationService {
+class ApplicationService(
+    private val repository: ApplicationRepository
+) {
+
+    fun getApplication(
+        id: String
+    ): Application? {
+        return repository.findApplicationByIntegrationId(id).getOrNull()
+    }
+
 }
--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/ApplicationController.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/ApplicationController.kt
@@ -1,6 +1,19 @@
 package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.tbank.itemsdeliverydemo.applicationsstorage.application.ApplicationService
+import ru.tbank.itemsdeliverydemo.applicationsstorage.mapper.ApplicationMapper
+import java.net.http.HttpResponse
+import java.util.UUID
 
 @RestController
 class ApplicationController
+@RequestMapping("/api/v1/applications")
+class ApplicationController(
+    private val service: ApplicationService,
+) {
+
+}
