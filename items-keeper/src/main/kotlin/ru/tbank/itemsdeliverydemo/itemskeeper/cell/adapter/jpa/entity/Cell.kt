0b68ffb99f1596c90f7bda2dc4f2f8f8274a881b
aleksandrledenev <ledenevsanok@gmail.com>
2025-01-20 02:49:16 +0300

    Создал jpa сущности

---
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cell/adapter/jpa/entity/Cell.kt
@@ -0,0 +1,4 @@
+package ru.tbank.itemsdeliverydemo.itemskeeper.cell.adapter.jpa.entity
+
+class Cell {
+}
---
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cellcharacteristics/adapter/jpa/entity/CellCharacteristics.kt
@@ -0,0 +1,4 @@
+package ru.tbank.itemsdeliverydemo.itemskeeper.cellcharacteristics.adapter.jpa.entity
+
+class CellCharacteristics {
+}
