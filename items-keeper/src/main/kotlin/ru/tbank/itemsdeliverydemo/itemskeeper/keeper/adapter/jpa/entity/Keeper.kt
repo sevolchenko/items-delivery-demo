b348be509978806b7b62bdd2c52f519957ec8f43
aleksandrledenev <ledenevsanok@gmail.com>
2025-01-20 02:49:16 +0300

    Создал jpa сущности

---
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/keeper/adapter/jpa/entity/Keeper.kt
@@ -0,0 +1,4 @@
+package ru.tbank.itemsdeliverydemo.itemskeeper.keeper.adapter.jpa.entity
+
+class Keeper {
+}
---
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/storagestatushistory/adapter/jpa/entity/StorageStatusHistory.kt
@@ -0,0 +1,4 @@
+package ru.tbank.itemsdeliverydemo.itemskeeper.storagestatushistory.adapter.jpa.entity
+
+class StorageStatusHistory {
+}
