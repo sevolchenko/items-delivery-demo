239daf00f433f80fb67e0f9648452132b480063a
aleksandrledenev <ledenevsanok@gmail.com>
2025-01-20 03:12:03 +0300

    Заполнил jpa сущности

--- items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cellcharacteristics/adapter/jpa/entity/CellCharacteristics.kt
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cellcharacteristics/adapter/jpa/entity/CellCharacteristics.kt
@@ -1,4 +1,20 @@
 package ru.tbank.itemsdeliverydemo.itemskeeper.cellcharacteristics.adapter.jpa.entity
 
-class CellCharacteristics {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "cell_characteristic")
+data class CellCharacteristics(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+
+    val length: Double = 0.0,
+    val height: Double = 0.0,
+    val width: Double = 0.0
+)
--- items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/keeper/adapter/jpa/entity/Keeper.kt
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/keeper/adapter/jpa/entity/Keeper.kt
@@ -1,4 +1,32 @@
 package ru.tbank.itemsdeliverydemo.itemskeeper.keeper.adapter.jpa.entity
 
-class Keeper {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import ru.tbank.itemsdeliverydemo.itemskeeper.cell.adapter.jpa.entity.Cell
+import ru.tbank.itemsdeliverydemo.itemskeeper.keeper.model.KeeperStatus
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "keeper")
+data class Keeper(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+    val productId: UUID? = null,
+
+    @ManyToOne
+    val cellId: Cell? = null,
+
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    var updatedAt: LocalDateTime? = null,
+
+    @Enumerated(EnumType.STRING)
+    var status: KeeperStatus = KeeperStatus.FREE,
+)
