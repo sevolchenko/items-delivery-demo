d2e3bbb396e9dd125f7b603a20bcdf556406d397
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-02-16 14:41:54 +0300

    Метод получения статуса заявки

--- applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/PublicApplicationController.kt
+++ applications-storage/src/main/kotlin/ru/tbank/itemsdeliverydemo/applicationsstorage/application/adapter/rest/PublicApplicationController.kt
@@ -1,7 +1,14 @@
 package ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 import ru.tbank.itemsdeliverydemo.applicationsstorage.application.ApplicationService
 import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.jpa.entity.Application
 import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest.dto.CreateApplicationRequest
+import ru.tbank.itemsdeliverydemo.applicationsstorage.application.adapter.rest.dto.PublicApplicationResponse
+import ru.tbank.itemsdeliverydemo.applicationsstorage.common.ErrorResponse
 import ru.tbank.itemsdeliverydemo.applicationsstorage.mapper.ApplicationMapper
 
 @RestController
@@ -18,6 +27,21 @@ class PublicApplicationController(
     private val mapper: ApplicationMapper
 ) {
 
+    @Operation(
+        summary = "Создать заявку",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Заявка успешно создана",
+                content = [
+                    Content(
+                        schema = Schema(implementation = PublicApplicationResponse::class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            )
+        ]
+    )
     @PostMapping
     fun createApplication(
         @RequestBody request: CreateApplicationRequest
@@ -25,8 +49,44 @@ class PublicApplicationController(
         return service.createApplication(request).toResponse()
     }
 
-    private fun Application.toResponse(): ResponseEntity<*> {
-        return ResponseEntity.ok(mapper.toPublicApplicationResponse(this))
+    @Operation(
+        summary = "Получить статус заявки",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Заявка успешно получена",
+                content = [
+                    Content(
+                        schema = Schema(implementation = PublicApplicationResponse::class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+                ]
+            ),
+            ApiResponse(
+               responseCode = "404",
+               description = "Заявка не найдена",
+               content = [
+                   Content(
+                       schema = Schema(implementation = ErrorResponse::class),
+                       mediaType = MediaType.APPLICATION_JSON_VALUE
+                   )
+               ]
+            )
+        ]
+    )
+    @GetMapping("/{id}/status")
+    fun getApplicationStatus(
+        @PathVariable id: String
+    ) : ResponseEntity<*> {
+        return service.getApplication(id).toResponse()
+    }
+
+    private fun Application?.toResponse(): ResponseEntity<*> {
+        return if (this == null) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse("Заявка не найдена"))
+        } else {
+            ResponseEntity.ok(mapper.toPublicApplicationResponse(this))
+        }
     }
 
 }
