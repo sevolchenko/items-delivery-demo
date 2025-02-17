3057e581d2765499a9588b10222378cf86fc761a
Aleksandr Ledenev <118855755+AleksandrLedenev@users.noreply.github.com>
2025-02-18 00:00:09 +0300

    Убрал неюзаный импорт

--- items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cell/adapter/jpa/rest/CellContoller.kt
+++ items-keeper/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemskeeper/cell/adapter/jpa/rest/CellContoller.kt
@@ -1,7 +1,6 @@
 package ru.tbank.itemsdeliverydemo.itemskeeper.cell.adapter.jpa.rest
 
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
