eec209fe71e7eb312ea20e1c4e5cdc69e44a4ab1
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-02-17 09:50:46 +0300

    Скопировал build.gradle из storage

--- operator-back/build.gradle.kts
+++ operator-back/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.spring.dependency-management")
     kotlin("jvm")
     kotlin("plugin.spring")
+    kotlin("kapt")
 }
 
 group = "ru.tbank.items-delivery-demo"
@@ -15,6 +16,19 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.liquibase:liquibase-core")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+
+    implementation("org.mapstruct:mapstruct")
+    kapt("org.mapstruct:mapstruct-processor")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.kafka:spring-kafka")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<Test> {
