5d52a87abb72907b43d0dfc740b96d93c04a87f7
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-02-21 20:15:47 +0300

    Добавил primary на objectMapper бин

--- common/src/main/kotlin/ru/tbank/itemsdeliverydemo/common/configuration/JacksonConfiguration.kt
+++ common/src/main/kotlin/ru/tbank/itemsdeliverydemo/common/configuration/JacksonConfiguration.kt
@@ -1,12 +1,14 @@
-package ru.tbank.itemsdeliverydemo.common
+package ru.tbank.itemsdeliverydemo.common.configuration
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class JacksonConfiguration {
 
     @Bean
+    @Primary
     fun objectMapper() = jacksonObjectMapper()
 }
