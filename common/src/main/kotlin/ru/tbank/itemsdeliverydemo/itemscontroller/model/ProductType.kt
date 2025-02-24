9122a84975a85cf7c439fdf1e3874be8caf69225
Aleksandr Ledenev <118855755+AleksandrLedenev@users.noreply.github.com>
2025-02-24 21:39:07 +0300

    указал правильные размеры ProductType

--- common/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/model/ProductType.kt
+++ common/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/model/ProductType.kt
@@ -4,10 +4,10 @@ enum class ProductType(
     val ruName: String,
     val size: Size
 ) {
-    NOTEBOOK("Блокнот", Size(10.0, 15.0, 20.0)),
-    PEN("Ручка", Size(1.0, 2.0, 3.0)),
-    STICKER("Стикер", Size(1.0, 2.0, 3.0)),
-    CARDHOLDER("Кардхолдер", Size(10.0, 15.0, 20.0))
+    NOTEBOOK("Блокнот", Size(150.0, 3.0, 200.0)),
+    PEN("Ручка", Size(1.0, 1.0, 13.0)),
+    STICKER("Стикер", Size(5.0, 1.0, 5.0)),
+    CARDHOLDER("Кардхолдер", Size(11.0, 1.0, 7.0))
 }
 
 data class Size(
