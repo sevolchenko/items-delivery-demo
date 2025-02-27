c5dbe9c00dc1061f30904c9c4736f3b040afa3db
Sergey Volchenko <s.volchenko@tinkoff.ru>
2025-02-27 17:30:28 +0300

    Add productType PACK

--- common/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/model/ProductType.kt
+++ common/src/main/kotlin/ru/tbank/itemsdeliverydemo/itemscontroller/model/ProductType.kt
@@ -6,8 +6,9 @@ enum class ProductType(
 ) {
     NOTEBOOK("Блокнот", Size(150.0, 3.0, 200.0)),
     PEN("Ручка", Size(1.0, 1.0, 13.0)),
-    STICKER("Стикер", Size(5.0, 1.0, 5.0)),
-    CARDHOLDER("Кардхолдер", Size(11.0, 1.0, 7.0))
+    STICKER("Стикерпак", Size(5.0, 1.0, 5.0)),
+    CARDHOLDER("Кардхолдер", Size(11.0, 1.0, 7.0)),
+    PACK("Пак", Size(150.0, 3.0, 200.0))
 }
 
 data class Size(
